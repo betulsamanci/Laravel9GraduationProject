b01921d1af5458a659d50ce41a57ec25f15f49a7
betulsamanci <betulsmncc@icloud.com>
2022-04-09 23:53:26 +0300

    Master Page Defined

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +13,26 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+//1-Do Something In Route
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+//2-Call View In Route
+Route::get('/welcome', function () {
     return view('welcome');
 });
+//3-Call Controller Function
+Route::get("/",[HomeController::class,"index"])->name("home");
+
+//4-Route-> Controller-> View
+Route::get('/test',[HomeController::class,'test'])->name('test');
+
+//5-Route With Parameters
+Route::get('/param/{id}/{number}',[HomeController::class,'param'])->name('param');
+
+//6-Route With Post
+Route::post('/save',[HomeController::class,'save'])->name('save');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
