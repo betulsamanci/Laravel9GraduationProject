e10cf3de6cb49abd352697844bae63f49be86032
betulsamanci <betulsmncc@icloud.com>
2022-06-12 23:11:13 +0300

    about page editing

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,47 +1,30 @@
 @extends('layouts.frontbase')
 
-@section('title',$setting->title)
+@section('title','About Us | '. $setting->title)
 @section('description',$setting->description)
 @section('keywords',$setting->keywords)
 @section('icon',Storage::url($setting->icon))
 
-
 @section('content')
 
 
-    <!DOCTYPE HTML>
-<html>
+    <div  id="breadcrumb">
+        <div class="container">
+            <ul class="breadcrumb">
+                <li><a href="{{route('home')}}">Home</a></li>
 
-<body class="is-preload">
-<div class="brand_color">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="titlepage">
-                    <h2>about</h2>
+                <li class="active"> /About</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    {!! $setting->aboutus !!}
                 </div>
             </div>
         </div>
     </div>
-
-</div>
-<!-- Wrapper -->
-<div id="wrapper">
-
-    <div id="main">
-
-    </div>
-
-    {!! $setting->about !!}
-
-</div>
-
-<!-- Scripts -->
-<script src="{{asset('assets')}}/assets/js/jquery.min.js"></script>
-<script src="{{asset('assets')}}/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="{{asset('assets')}}/assets/js/jquery.scrolly.min.js"></script>
-<script src="{{asset('assets')}}/assets/js/jquery.scrollex.min.js"></script>
-<script src="{{asset('assets')}}/assets/js/main.js"></script>
-</body>
-</html>
 @endsection
