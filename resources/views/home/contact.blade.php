a67c475b72fac9ca9e02538fad6d5320513bf238
betulsamanci <betulsmncc@icloud.com>
2022-06-12 23:52:02 +0300

    contact page ımplementatiton

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -7,13 +7,12 @@
 
 @section('content')
 
-
     <div  id="breadcrumb">
         <div class="container">
             <ul class="breadcrumb">
                 <li><a href="{{route('home')}}">Home</a></li>
 
-                <li class="active"> /References</li>
+                <li class="active"> /Contact</li>
             </ul>
         </div>
     </div>
@@ -22,9 +21,61 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    {!! $setting->references !!}
+                    {!! $setting->contact  !!}
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="brand_color">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="titlepage">
+                        <h2>Contact Us</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <form  id="checkout-form" action="{{route("storemessage")}}" class="clearfix" method="post">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                <input class="form-control" placeholder="Name&Surname" type="text" name="name">
+                            </div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                <input class="form-control" placeholder="Email" type="text" name="Email">
+                            </div>
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                                <input class="form-control" placeholder="Phone Number" type="text" name="phone">
+                            </div>
+                            <div class=" col-md-12">
+                                <input class="form-control" placeholder="Subject" type="text" name="Subject">
+                            </div>
+                            <div class="col-md-12">
+                                <textarea class="textarea" placeholder="Message"></textarea>
+                            </div>
+                            <div class=" col-md-12">
+                                <button class="send">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{asset('assets')}}/assets/js/jquery.min.js"></script>
+    <script src="{{asset('assets')}}/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('assets')}}/assets/js/jquery.scrolly.min.js"></script>
+    <script src="{{asset('assets')}}/assets/js/jquery.scrollex.min.js"></script>
+    <script src="{{asset('assets')}}/assets/js/main.js"></script>
+
 @endsection
